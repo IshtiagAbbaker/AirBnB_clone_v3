50e331766d7794a3073ff487912ac142db8169f8
IshtiagAbbaker <ishtiagabdelmomen@gmail.com>
2024-04-27 10:38:13 -0700

    REST

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -85,7 +85,7 @@ class TestBaseModel(unittest.TestCase):
         tic = datetime.now()
         inst1 = BaseModel()
         toc = datetime.now()
-        self.assertTrue(tic <= inst1.created_at <= toc, f"Instance created_at ({inst1.created_at}) not within expected range: {tic} - {toc}")
+        self.assertTrue(tic <= inst1.created_at <= toc)
         time.sleep(1e-4)
         tic = datetime.now()
         inst2 = BaseModel()
